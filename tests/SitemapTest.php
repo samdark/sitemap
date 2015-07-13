56603af3067678be3f5dd7e0dc780ae4ae1ec9be
Alexander Makarov <sam@rmcreative.ru>
2015-07-13 14:22:29 +0300

    More code polish

--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -75,4 +75,16 @@ class SitemapTest extends \PHPUnit_Framework_TestCase
 
         unlink($fileName);
     }
+
+    public function testLocationValidation()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+
+        $fileName = __DIR__ . '/sitemap.xml';
+        $sitemap = new Sitemap($fileName);
+        $sitemap->addItem('http://example.com/mylink1');
+        $sitemap->addItem('mylink2', time());
+
+        unlink($fileName);
+    }
 }
